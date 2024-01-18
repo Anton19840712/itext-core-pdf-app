90d7319275443d2b788b9510353952569252375a
Anton Gridushko <gridushko.anton@yahoo.com>
2024-01-18 10:29:46 +0300

    Add fonts.

--- ItextTest/Program.cs
+++ ItextTest/Program.cs
@@ -25,7 +25,7 @@ canvas.AddImageFittedIntoRectangle(ImageDataFactory.Create(certPath), pageSize, 
 
 // Add header section:
 var compName = "WERONICA.LIFE";
-var text1 = new Paragraph(compName)
+var text1 = new Paragraph(new Text(compName).SetBold().SetFontSize(20))
 	.SetTextAlignment(TextAlignment.CENTER)
 	.SetRelativePosition(0, 200, 0, 0);
 
@@ -38,14 +38,24 @@ var date1 = new DateOnly(2023, 09, 28);
 var date2 = new DateOnly(2024, 09, 28);
 var date3 = new DateOnly(2023, 02, 16);
 
-var centerText = $"Настоящим сертификатом подтверждается"+
-                 $"что владельцем домена {compName}\n является {name}\n\n\n" +
-                 $"Дата первичной регистрации домена {date1.ToString("dd MMMM yyyy года", new CultureInfo("ru-RU"))}\n" +
-                 $"Дата окончания срока регистрации домена {date2.ToString("dd MMMM yyyy года", new CultureInfo("ru-RU"))}\n\n" +
-                 $"Дата выдачи сертификата {date3.ToString("dd MMMM yyyy года", new CultureInfo("ru-RU"))}";
+var middleTextFirstRow = $"Настоящим сертификатом подтверждается, что владельцем домена ";
+var middleTextFirstRowParam1 = $"{compName}\n";
+var middleTextSecondRow = "является ";
+var middleTextSecondRowParam1 = $"{name}\n\n\n";
+var middleTextThirdRow = $"Дата первичной регистрации домена {date1.ToString("dd MMMM yyyy года", new CultureInfo("ru-RU"))}\n";
+var middleTextFourthRow = $"Дата окончания срока регистрации домена {date2.ToString("dd MMMM yyyy года", new CultureInfo("ru-RU"))}\n\n";
+var middleTextFifthRow = $"Дата выдачи сертификата {date3.ToString("dd MMMM yyyy года", new CultureInfo("ru-RU"))}";
 
-var text2 = new Paragraph(centerText)
+var text2 = new Paragraph()
+	.Add(middleTextFirstRow)
+	.Add(new Text(middleTextFirstRowParam1).SetBold().SetFontSize(8))
+	.Add(middleTextSecondRow)
+	.Add(new Text(middleTextSecondRowParam1).SetBold())
+	.Add(middleTextThirdRow)
+	.Add(middleTextFourthRow)
+	.Add(new Text(middleTextFifthRow).SetBold())
 	.SetFont(font)
+	.SetFontSize(12)
 	.SetRelativePosition(0, 300, 0, 0);
 
 
